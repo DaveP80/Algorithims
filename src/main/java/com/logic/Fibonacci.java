66426be6b493613915d89f08aac97fd141cd1b15
DaveP80 <davidpaq1@gmail.com>
2022-09-25 19:35:18 -0400

    "mean"

--- src/main/java/com/logic/Fibonacci.java
+++ src/main/java/com/logic/Fibonacci.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 public class Fibonacci {
     public static List<BigInteger> dict = new ArrayList<BigInteger>();
+
     public static List<BigInteger> printFibonacciRecursion(int num) {
 
         if (num == 1) {
@@ -19,5 +20,48 @@ public class Fibonacci {
         }
         return dict;
     }
-}
+
+    public static BigInteger mean (ArrayList<BigInteger> dict) {
+        BigInteger total = BigInteger.valueOf(0);
+        BigInteger size = BigInteger.valueOf(dict.size());
+
+        for (int i = 2; i < dict.size(); i++) {
+
+            if (i % 2 == 0) {
+
+                BigInteger a
+                        = new BigInteger(String.valueOf(dict.get(i)));
+//            BigInteger currentNum = dict.get(i);
+                BigInteger b
+                        = new BigInteger(String.valueOf(dict.get(i + 1)));
+
+                total = total.add(a.add(b));
+            }
+
+        }return total.divide(size);
+    }
+
+//        public double standardDeviation (ArrayList < BigInteger > dict) {
+//
+//            // Step 1:
+//            double mean = mean(dict);
+//            double temp = 0;
+//
+//            for (int i = 0; i < dict.size(); i++) {
+//                BigInteger val = dict.get(i);
+//
+//                // Step 2:
+//                double squrDiffToMean = Math.pow(val - mean, 2);
+//
+//                // Step 3:
+//                temp += squrDiffToMean;
+//            }
+//
+//            // Step 4:
+//            double meanOfDiffs = (double) temp / (double) (dict.size());
+//
+//            // Step 5:
+//            return Math.sqrt(meanOfDiffs);
+//        }
+    }
 
